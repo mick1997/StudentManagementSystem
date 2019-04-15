e103355e8ddd3693cabceff697a3d524853f0836
jackfeng <mickjack2013@gmail.com>
2019-04-15 01:53:12 -0400

    fixed some features

--- src/studentmanagementsystem/LoginFormController.java
+++ src/studentmanagementsystem/LoginFormController.java
@@ -1,17 +1,35 @@
 package studentmanagementsystem;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author Luo(Jack) Feng
  */
 public class LoginFormController implements Initializable {
+    
+    @FXML
+    private JFXRadioButton stuCheckBox;
+    
+    @FXML
+    private JFXRadioButton proCheckBox;
+    
+    @FXML
+    private TextField emailTextField;
+    
+    @FXML
+    private TextField passwordTextfield;
+    
+    @FXML
+    private JFXButton conBTn;    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
