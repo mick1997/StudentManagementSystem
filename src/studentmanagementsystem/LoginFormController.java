052efb2afd71caf69d27a3703739bfb1b522ddac
jackfeng <mickjack2013@gmail.com>
2019-04-15 16:12:44 -0400

    Add more features

--- src/studentmanagementsystem/LoginFormController.java
+++ src/studentmanagementsystem/LoginFormController.java
@@ -2,13 +2,20 @@ package studentmanagementsystem;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 /**
  *
@@ -23,13 +30,57 @@ public class LoginFormController implements Initializable {
     private JFXRadioButton proCheckBox;
     
     @FXML
+    private ToggleGroup loginType;
+    
+    @FXML
     private TextField emailTextField;
     
     @FXML
     private TextField passwordTextfield;
     
     @FXML
-    private JFXButton conBTn;    
+    private JFXButton conBTn;
+    
+    public static Student student;
+    public static Professor professor;
+    
+    public void onClickSignin(ActionEvent a) {
+        
+        try {
+            if ("Student".equals(((JFXRadioButton) loginType.getSelectedToggle()).getText())) {
+                
+            }
+            else if ("Professor".equals(((JFXRadioButton) loginType.getSelectedToggle()).getText())) {
+                
+            }
+        } 
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onBtnContinue(ActionEvent e) throws Exception {
+        
+        if (emailTextField.getText().equals("Student") && passwordTextfield.getText().equals("123")) {
+            Stage primaryStage = new Stage();
+            Parent loginFace = FXMLLoader.load(getClass().getResource("/StudentManagementSystem/StudentDashBoard.fxml"));
+            Scene scene = new Scene(loginFace, 400, 400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+//            Stage app = (Stage)((Node) e.getSource()).getScene().getWindow();
+//            app.setScene(new Scene(loginFace));
+//            app.show();
+        }
+        else if (emailTextField.getText().equals("Professor") && passwordTextfield.getText().equals("456")) {
+            System.out.println("hello");
+            Stage primaryStage = new Stage();
+            Parent loginFace = FXMLLoader.load(getClass().getResource("/StudentManagementSystem/ProfessorDashBoard.fxml"));
+            Scene scene = new Scene(loginFace, 400, 400);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
